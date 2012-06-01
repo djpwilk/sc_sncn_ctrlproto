43cfa16a864d593c4026d72164fd05762140aedc
Frank Jeschke <jes@hostis.de>
2012-06-01 11:26:40 +0200

    Added documentation on hwo to use the simple channel protocol

--- module_ethercat/src/foe_chan.h
+++ module_ethercat/src/foe_chan.h
@@ -3,7 +3,29 @@
 
 /* defines for file access over channel */
 
-/* commands from the caller */
+/**
+ * commands from the caller
+ *
+ * The following commands are recognized:
+ * FOE_FILE_OPEN <filename>
+ * open specified file for reading, replies FOE_FILE_OK on success or FOE_FILE_ERROR on error.
+ *
+ * FOE_FILE_CLOSE
+ * Finish file access operation
+ *
+ * FOE_FILE_READ <size>
+ * read <size> bytes from previously opened file
+ *
+ * FOE_WRITE <size> <data>
+ *** currently unsupported ***
+ *
+ * FOE_FILE_SEEK <pos>
+ * set filepointer to (absolute) position <pos>, the next read/write operation will start from there.
+ * With FOE_FILE_SEEK 0 the file pointer is rewind to the beginning of the file.
+ *
+ * FOE_FILE_FREE
+ * This command will erase the file (or files), so further file access is possible.
+ */
 #define FOE_FILE_OPEN      10
 #define FOE_FILE_READ      11
 #define FOE_FILE_WRITE     12
