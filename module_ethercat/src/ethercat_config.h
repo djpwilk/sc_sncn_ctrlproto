4b831a2ff32e6249b9be8f3ef00757074f87f482
Frank Jeschke <jes@hostis.de>
2012-12-14 23:32:42 +0100

    Added simple config file

---
+++ module_ethercat/src/ethercat_config.h
@@ -0,0 +1,13 @@
+/**
+ * A simple configuration file for the ethercat module to activate or disable specific features.
+ */
+
+#ifndef ETHERCAT_CONFIG_H
+#define ETHERCAT_CONFIG_H
+
+/* comment in the features used here, this is a stub! */
+//#define ECAT_HAS_EOE  1
+//#define ECAT_HAS_FOE  1
+//#define ECAT_HAS_VOE  1
+
+#endif /* ETHERCAT_CONFIG_H */
