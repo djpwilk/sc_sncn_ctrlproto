4ad8926cc2a928cbdfd6ba98df0ef36b1e663844
Christian Holl <choll@synapticon.com>
2013-03-12 17:13:49 +0100

    Adds some additional documentation

--- module_ctrlproto_xmos/src/ctrlproto.h
+++ module_ctrlproto_xmos/src/ctrlproto.h
@@ -20,10 +20,16 @@ typedef struct
  * This function receives channel communication from the ctrlproto_pdo_handler_thread
  * It updates the referenced values according to the command and has to be placed
  * inside the control loop.
+ * This function is not considered as stand alone thread! It's for being executed in
+ * the motor control thread
  *
  * @param pdo_out					The channel for outgoing process data objects
  * @param pdo_in					The channel for incoming process data objects
+ * @param InOut						The struct for exchanging data with the motor control functions
  */
 void ctrlproto_protocol_handler_function(chanend pdo_out, chanend pdo_in, ctrl_proto_values_t &InOut);
 
+/**
+ * This function initializes a struct from the type of ctrl_proto_values_t
+ */
 void init_ctrl_proto(ctrl_proto_values_t &InOut);
